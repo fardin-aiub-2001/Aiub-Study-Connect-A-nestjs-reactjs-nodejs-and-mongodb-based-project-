5479199fb6dec6dfa779aeb4d844bf69a0ecc6b9
Sadia Khandaker Moumita <sadiakhandakar685@gmail.com>
2026-06-26 16:50:13 +0600

    Implemented admin

--- aiub_study_connect/src/admin/admin.controller.ts
+++ aiub_study_connect/src/admin/admin.controller.ts
@@ -1,10 +1,35 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AdminService } from './admin.service';
-
 
 @Controller('admin')
 export class AdminController {
-  constructor(private readonly adminService: AdminService) {}
+    constructor(
+        private readonly adminService: AdminService) {}
+    @Get()
+    getAdmin(): string {
+        return this.adminService.getAdmin();
+    }
+    @Get('allusers')
+    getAllUsers(): object {
+        return this.adminService.getAllUsers();
+    }
+    @Get('getuserbyid/:id')
+    getUserByID(@Param('id') id:number): object {
+        return this.adminService.getUserByID(id);
+    }
+    @Get('getuserbyid/:id/getbyrole/:role')
+    getUserByIDAndRole(@Param('id') id:number,@Param('role') role:string
+    ): object {
+        return this.adminService.getUserByIDAndRole(id,role);
+    }
+    @Get('getuser')
+    getUser(
+        @Query('id') id:number): object {
+        return this.adminService.getUser(id);
+    }
+    @Get('getuserbyidandrole')
+    getUserByIDAndRoleQuery( @Query('id') id:number, @Query('role') role:string): object {
+        return this.adminService.getUserByIDAndRoleQuery(id,role);
+    }
 
-  
 }
--- aiub_study_connect/src/admin/admin.service.ts
+++ aiub_study_connect/src/admin/admin.service.ts
@@ -1,7 +1,25 @@
 import { Injectable } from '@nestjs/common';
 
-
 @Injectable()
 export class AdminService {
-  
+    getAdmin(): string {
+        return "Welcome Admin";
+    }
+    getAllUsers(): object {
+        return {totalUsers: 500,students: 400,faculty: 50,administrative: 50};
+    }
+    getUserByID(id:number): object {
+        return {id:id,name:"Moumita",role:"Student"};
+    }
+    getUserByIDAndRole(id:number, role:string): object {
+        return {id:id,role:role
+        };
+    }
+    getUser(id:number): object {
+        return {id:id,status:"Found"};
+    }
+    getUserByIDAndRoleQuery(id:number, role:string): object {
+        return {id:id,role:role,status:"Found"};
+    }
+
 }
