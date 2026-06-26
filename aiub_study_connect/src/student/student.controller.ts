06be1254e6d7d22a9c360d7b532a56fdf9c3e367
Fardin <fardinahmedrocktim@gmail.com>
2026-06-26 06:41:14 +0600

    adding some new config

--- aiub_study_connect/src/student/student.controller.ts
+++ aiub_study_connect/src/student/student.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { StudentService } from './student.service';
 
 @Controller('student')
 export class StudentController {
   constructor(private readonly studentService: StudentService) {}
+
+  @Get()
+  getStudent(): string {
+    return this.studentService.getstudent();
+  }
+  //http://localhost:3000/student
+
+  @Get('getstudentbyid/:id')
+  getStudentById(@Param('id') id: number): string {
+    return this.studentService.getStudentById(id);
+  }
+  //http://localhost:3000/student/getstudentbyid/1
+  //string means the method must return a string.
+
+  @Get('getstudentbyid/:id/getstudentbyname/:name')
+  getStudentByIdAndName(@Param('id') id: number, @Param('name') name: string): object {
+    return this.studentService.getStudentByIdAndName(id, name);
+  }
+  //http://localhost:3000/student/getstudentbyid/1/getstudentbyname/farhan
+  //object means the method must return an object (key–value pairs).
+  //return structured data (JSON).
+  //The @Param() decorator is used to extract the parameters from the URL and pass them to the method.
+  //The @Get() decorator is used to define the route for the method.
+  //The @Controller() decorator is used to define the controller for the route.
+  //The StudentController class is responsible for handling the HTTP requests and responses for the student resource.
+  
 }
--- aiub_study_connect/src/student/student.service.ts
+++ aiub_study_connect/src/student/student.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+    getstudent() {
+        return "All students data";
+    }
+
+    getStudentById(id: number):string{
+        return `Student data for ID: ${id}`;
+    }
+
+    getStudentByIdAndName(id:number, name:string):object{
+        return {"id": id, "name": name};
+    }
+}
